c1e290df69aaebeaff34471439e303f59a89b92d
David J. Bianco <davidjbianco@gmail.com>
2020-07-09 11:26:13 -0400

    Initial working domaintools module. Most API features not yet present.

---
+++ huntlib/domaintools.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+from configparser import ConfigParser
+
+from domaintools import API
+
+from tqdm import tqdm
+
+import os.path
+
+__all__ = ['DomainTools']
+
+class DomainTools(object):
+
+    _DEFAULT_CONFIG_FILE = os.path.expanduser("~/.huntlibrc")
+
+    _handle = None # API Handle
+    _account_information = None # Cached information about the API user's account
+    _available_api_calls = None # Cached list of API endpoints the account has access to
+
+    def __init__(self, *args, **kwargs):
+
+        # FIXME: this should be a huntlib-wide config capability
+        # Read our config file, if it exists
+        config = ConfigParser()
+        config.read(self._DEFAULT_CONFIG_FILE)
+        
+        api_username = ""
+        api_key = ""
+
+        if 'api_username' in kwargs:
+            api_username = kwargs['api_username']
+        elif config.has_option('domaintools', 'api_username'):
+            api_username = config.get('domaintools', 'api_username')
+
+        if 'api_key' in kwargs:
+            api_key = kwargs['api_key']
+        elif config.has_option('domaintools', 'api_key'):
+            api_key = config.get('domaintools', 'api_key')
+
+        self.authenticate(
+            api_username=api_username,
+            api_key=api_key
+        )
+
+        self._account_information = self.account_information(force_refresh=True)
+        self._available_api_calls = self.available_api_calls(force_refresh=True)
+
+
+    def authenticate(self, api_username="", api_key=""):
+        """
+        Authenticate to the DomainTools API. 
+
+        Params:
+          api_username: a string containing the account name to log in with
+          api_key: a string containing the API secret key (password) to log in with
+
+        Errors:
+          Will raise ValueError if the api_username or api_key are not provided
+          or are of the wrong type.
+        """
+        # Sanity check the provided authentication info
+        if not api_username:
+            raise ValueError("You must supply a value for 'api_username'.")
+        elif not isinstance(api_username, str):
+            raise ValueError("The 'api_username' field must be a string.")
+
+        if not api_key:
+            raise ValueError("You must supply a value for 'api_key'.")
+        elif not isinstance(api_key, str):
+            raise ValueError("The 'api_key' field must be a string.")
+
+
+        # Actually authenticate now
+        self._handle = API(
+            api_username,
+            api_key
+        )
+
+
+    def account_information(self, force_refresh=False, **kwargs):
+
+        if self._account_information and not force_refresh:
+            return self._account_information
+
+        res = self._handle.account_information(**kwargs)
+
+        if res:
+            info = dict()
+            for item in res:
+                id = item.pop('id')
+                info[id] = dict(item)
+            self._account_information = info
+
+        return self._account_information
+
+    def available_api_calls(self, force_refresh=False, **kwargs):
+
+        if self._available_api_calls and not force_refresh:
+            return self._available_api_calls
+
+        res = self._handle.available_api_calls(**kwargs)
+
+        if res:
+            self._available_api_calls = list(res)
+
+        return self._available_api_calls
+
+    def whois(self, query=None, **kwargs):
+
+        if not query:
+            raise ValueError("You must supply either a domain or an IP address.")
+        elif not isinstance(query, str):
+            raise ValueError("The query parameter must be either a 'str' or a 'bytes' object.")
+        
+        whois_info = dict(list(self._handle.whois(query, **kwargs)))
+
+        return whois_info
+
+    def parsed_whois(self, query=None, flatten=False, **kwargs):
+
+        if not query:
+            raise ValueError(
+                "You must supply either a domain or an IP address.")
+        elif not (isinstance(query, str) or isinstance(query, bytes)):
+            raise ValueError(
+                "The query parameter must be either a 'str' or a 'bytes' object.")
+
+        whois_info = dict(list(self._handle.parsed_whois(query, **kwargs)))
+        
+        if flatten:
+            # Normalize the nested dictionary keys into a single level.
+            # This is a built-in method of the DomainTools ParsedWhois() 
+            # results class.
+            whois_info = pd.json_normalize(whois_info).iloc[0].to_dict()
+
+        return whois_info
+
+    def brand_monitor(self, query=None, **kwargs):
+        if not query:
+            raise ValueError("You must specify a query pattern.")
+        elif isinstance(query, list):
+            query = "|".join(query)
+        elif not isinstance(query, str):
+            raise ValueError("The 'query' parameter must be either a string or a list of strings.")
+
+        return list(self._handle.brand_monitor(query, **kwargs))
+
+    def enrich(self, df=None, column=None, prefix='dt_whois.', progress_bar=False, fields=None):
+        if df is None:
+            raise ValueError("You must supply a pandas DataFrame in the 'df' parameter.")
+        elif not isinstance(df, pd.core.frame.DataFrame):
+            raise ValueError("The argument for the 'df' parameter must be a pandas DataFrame.")
+
+        if not column:
+            raise ValueError("You must supply a column name to enrich.")
+        elif not isinstance(column, str):
+            raise ValueError("The column name must be a 'str'.")
+        elif not column in df.columns:
+            raise ValueError(f"The column '{column}' does not exist in the frame.")
+
+        if prefix is None or not isinstance(prefix, str):
+            raise ValueError("The column name prefix must be a 'str'.")
+
+        if fields is not None and not isinstance(fields, list):
+            raise ValueError("The 'fields' parameter must be a list of strings.")
+
+        if progress_bar:
+            tqdm.pandas(desc='Enriching WHOIS')
+            apply_func = df[column].progress_apply
+        else:
+            apply_func = df[column].apply
+
+        # Enrich with parsed WHOIS data
+        enrichment_df = apply_func(
+            lambda d: pd.Series(self.parsed_whois(d, flatten=True))
+        )
+
+        # If we asked for only certain fields, filter for those
+        if fields:
+            enrichment_df = enrichment_df[fields]
+
+        enrichment_df = enrichment_df.add_prefix(prefix)
+
+        df = df.merge(
+            enrichment_df,       
+            left_index=True, 
+            right_index=True
+        )
+
+        return df
+
---
+++ tests/test_domaintools.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+from multiprocessing import cpu_count
+from unittest import TestCase
+
+from huntlib.domaintools import DomainTools
+
+import pandas as pd 
+import numpy as np
+
+class TestDomainTools(TestCase):
+
+    _handle = None
+
+    @classmethod
+    def setUpClass(self):
+        '''
+        Authenticate to DomainTools once, and reuse that connection for all the 
+        tests in this module.
+        '''
+
+        # This uses the default creds stored in ~/.huntlibrc
+        dt = DomainTools()
+
+        self._handle = dt
+
+    def test_account_information(self):
+        limits = self._handle.account_information()
+
+        self.assertIsInstance(
+            limits, 
+            dict,
+            f"The return type of account_information was {type(limits)} not 'dict'."
+        )
+
+        self.assertGreater(
+            len(limits),
+            0,
+            "account_information() did not return any information."
+        )
+
+    def test_available_api_calls(self):
+
+        api_calls = self._handle.available_api_calls()
+
+        self.assertIsInstance(
+            api_calls,
+            list,
+            f"The return type of available_api_calls was {type(api_calls)} not 'list'."
+        )
+
+        self.assertGreater(
+            len(api_calls),
+            0,
+            "available_api_calls() did not return any information."
+        )
+
+    def test_whois_domain(self):
+
+        whois = self._handle.whois('google.com')
+
+        self.assertIsInstance(
+            whois,
+            dict,
+            f"The return type of whois() was {type(whois)} not 'dict'."
+        )
+
+        self.assertIn(
+            "registrant",
+            whois,
+            "Couldn't find the 'registrant' field in whois data."
+        )
+
+        self.assertEqual(
+            "Google LLC",
+            whois['registrant'],
+            "The registrant information does not seem to be correct."
+        )
+
+    def test_whois_ipv4(self):
+
+        whois = self._handle.whois('172.217.164.164')
+
+        self.assertIsInstance(
+            whois,
+            dict,
+            f"The return type of whois() was {type(whois)} not 'dict'."
+        )
+
+        self.assertIn(
+            "registrant",
+            whois,
+            "Couldn't find the 'registrant' field in whois data."
+        )
+
+        self.assertEqual(
+            "Google LLC",
+            whois['registrant'],
+            "The registrant information does not seem to be correct."
+        )
+
+    def test_parsed_whois_domain(self):
+
+        whois = self._handle.parsed_whois('google.com')
+
+        self.assertIsInstance(
+            whois,
+            dict,
+            f"The return type of whois() was {type(whois)} not 'dict'."
+        )
+
+        self.assertIn(
+            "registrant",
+            whois,
+            "Couldn't find the 'registrant_org' field in whois data."
+        )
+
+        self.assertEqual(
+            "Google LLC",
+            whois['registrant'],
+            "The registrant information does not seem to be correct."
+        )
+
+        self.assertIn(
+            'registration',
+            whois,
+            "Couldn't find the 'registration' field in the parsed whois data."
+        )
+
+        self.assertIn(
+            'created',
+            whois['registration'],
+            "Couldn't find the 'created' field in the 'registration' dict."
+        )
+
+        self.assertEqual(
+            "1997-09-15",
+            whois['registration']['created'],
+            "The registration create information does not seem to be correct."
+        )
+
+    def test_parsed_whois_ipv4(self):
+
+        whois = self._handle.parsed_whois('8.8.8.8')
+
+        self.assertIsInstance(
+            whois,
+            dict,
+            f"The return type of whois() was {type(whois)} not 'dict'."
+        )
+
+    def test_enrich(self):
+
+        df = pd.DataFrame(['google.com', 'microsoft.com', '8.8.8.8'], columns=["domain"])
+
+        enriched_df = self._handle.enrich(df, column='domain')
+
+        self.assertEqual(
+            enriched_df.shape[1],
+            74,
+            "Enriched DataFrame does not have the correct number of columns."
+        )
+
+        self.assertIn(
+            'dt_whois.parsed_whois.contacts.registrant.org',
+            enriched_df.columns,
+            "Could not find the 'dt_whois.parsed_whois.contacts.registrant.org' column in the enriched frame."
+        )
+
+    def test_brand_monitor(self):
+
+        # Because this depends so much on which domains are registered
+        # each day, it's hard to find a real term that has guaranteed
+        # matches.  So we just look for anything with the letter 'a' in
+        # it, which is virtualy assured.
+        domains = self._handle.brand_monitor('a')
+
+        self.assertIsInstance(
+            domains,
+            list,
+            f"The return from brand_monitor() was a {type(domains)}, not a list."
+        )
+
+        self.assertGreater(
+            len(domains),
+            0,
+            "The brand_monitor search returned no results."
+        )
